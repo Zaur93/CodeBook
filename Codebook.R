1aed68f4c2185d698e66acad923e979a99f3b3ce
Zaur93 <60624912+Zaur93@users.noreply.github.com>
2020-03-30 09:58:32 +0300

    Add files via upload

---
+++ Codebook.R
@@ -0,0 +1,4 @@
+## Extract only the measurements on the mean and standard deviation for each measurement
+## Assign each data to variables
+## x_test, y_test
+## Merges the training and the test x_train,x_test,y_train,y_test
